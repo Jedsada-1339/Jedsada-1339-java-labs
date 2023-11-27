ba9f2e1e6a844eac0f84bf3f0f009133456077f7
Jedsada-1339 <jedsada.si@kkumail.com>
2023-11-27 14:05:01 +0700

    c

--- Sirikhojornsombut/Jedsada/lab3/NumberGuessingMethodGame.java
+++ Sirikhojornsombut/Jedsada/lab3/NumberGuessingMethodGame.java
@@ -32,7 +32,7 @@ public class NumberGuessingMethodGame {
 
 
             if (guess == answer) {
-                System.out.println("Congratulations! You guessed the correct number.");
+                System.out.println("Congratulations! You guessed the correct numbers.");
                 break;
             } else if (guess < 1 || guess > 20) {
                 System.out.println("Enter an integer between 1 and 20: ");
