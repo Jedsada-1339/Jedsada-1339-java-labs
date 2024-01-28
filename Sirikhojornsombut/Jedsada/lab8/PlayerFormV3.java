ad289dd5508124906c6c179f543226018492df26
Jedsada-1339 <jedsada.si@kkumail.com>
2024-01-28 20:53:38 +0700

    update lab8

---
+++ Sirikhojornsombut/Jedsada/lab8/PlayerFormV3.java
@@ -0,0 +1,73 @@
+package sirikhojornsombut.jedsada.lab8; //sirikhojornsombut.jedsada.lab7.PlayerFormV2
+
+import javax.swing.*;
+
+public class PlayerFormV3 extends sirikhojornsombut.jedsada.lab7.PlayerFormV2 {
+    // Constructor for PlayerFormV3
+    public PlayerFormV3() {
+        setTitle("Player Form V3"); // Set the title for PlayerFormV3
+    }
+
+    // Override addMenus to add menu bar with specified menus
+    protected void addMenus() {
+        JMenuBar menuBar = new JMenuBar(); // Create a menu bar
+
+        // Menu "File" with four menu items
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(exitMenuItem);
+
+        // Menu "Config" with two menu items
+        JMenu configMenu = new JMenu("Config");
+        
+        JMenu colorMenu = new JMenu("Color");
+        JMenuItem redMenuItem = new JMenuItem("Red");
+        JMenuItem greenMenuItem = new JMenuItem("Green");
+        JMenuItem blueMenuItem = new JMenuItem("Blue");
+        colorMenu.add(redMenuItem);
+        colorMenu.add(greenMenuItem);
+        colorMenu.add(blueMenuItem);
+
+        JMenuItem sizeMenu = new JMenu("Size");
+        JMenuItem size16MenuItem = new JMenuItem("16");
+        JMenuItem size20MenuItem = new JMenuItem("20");
+        JMenuItem size24MenuItem = new JMenuItem("24");
+        sizeMenu.add(size16MenuItem);
+        sizeMenu.add(size20MenuItem);
+        sizeMenu.add(size24MenuItem);
+
+        configMenu.add(colorMenu);
+        configMenu.add(sizeMenu);
+
+        // Add menus to the menu bar
+        menuBar.add(fileMenu);
+        menuBar.add(configMenu);
+
+        // Set the menu bar for the frame
+        setJMenuBar(menuBar);
+    }
+
+    // Static method to create and show the GUI for PlayerFormV3
+    public static void createAndShowGUI() {
+        PlayerFormV3 playerForm = new PlayerFormV3();
+        playerForm.addComponents();
+        playerForm.addMenus(); // Add the menu bar
+        playerForm.setFrameFeatures();
+    }
+
+    // Main method to run the application for PlayerFormV3
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
