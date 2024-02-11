df485ee3282b2727cc17e25ebcdfa52077457fa3
Jedsada-1339 <jedsada.si@kkumail.com>
2024-02-11 12:30:36 +0700

    update lab9

---
+++ Sirikhojornsombut/Jedsada/lab9/PlayerFormV6.java
@@ -0,0 +1,54 @@
+package sirikhojornsombut.jedsada.lab9;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+
+public class PlayerFormV6 extends sirikhojornsombut.jedsada.lab8.PlayerFormV5 implements ActionListener {
+    
+    public PlayerFormV6() {
+        setTitle("Player Form V6"); // Set the title for PlayerFormV4
+    }
+
+    public void addListeners(){
+        submitButton.addActionListener(this);
+        resetButton.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Object srcObject = e.getSource();
+        if (srcObject == submitButton) {
+            JOptionPane.showMessageDialog(this, "hello");
+        }
+    }
+    
+
+    // Override addComponents to add new components to the frame
+    protected void addComponents() {
+        super.addComponents(); // Call the addComponents method from the parent class
+    }
+
+    protected void addMenus(){
+        super.addMenus();
+    }
+    
+    public static void createAndShowGUI() {
+        PlayerFormV6 playerForm = new PlayerFormV6();
+        playerForm.addComponents();
+        playerForm.addMenus();
+        playerForm.addListeners();
+        playerForm.setFrameFeatures();
+    }
+
+    // Main method to run the application for PlayerFormV5
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
