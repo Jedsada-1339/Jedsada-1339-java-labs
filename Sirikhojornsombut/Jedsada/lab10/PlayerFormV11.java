d4d844f5913c506dec35a7579185f823278eb574
Jedsada-1339 <jedsada.si@kkumail.com>
2024-02-19 22:04:44 +0700

    update lab10

--- Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
+++ Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
@@ -1,11 +1,15 @@
 package sirikhojornsombut.jedsada.lab10;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JColorChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public class PlayerFormV11 extends PlayerFormV10 {
@@ -16,17 +20,17 @@ public class PlayerFormV11 extends PlayerFormV10 {
         setTitle("Player Form V11");
     }
 
-    protected void addMenus(){
+    protected void addMenus() {
         JMenuBar menuBar = new JMenuBar(); // Create a menu bar
 
         // Menu "File" with four menu items
         JMenu fileMenu = new JMenu("File");
         ImageIcon newIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/New-icon.png");
-        newMenuItem = new JMenuItem("New",newIcon);
+        newMenuItem = new JMenuItem("New", newIcon);
         ImageIcon openIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/Open-icon.png");
-        openMenuItem = new JMenuItem("Open",openIcon);
+        openMenuItem = new JMenuItem("Open", openIcon);
         ImageIcon saveIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/Save-icon.png");
-        saveMenuItem = new JMenuItem("Save",saveIcon);
+        saveMenuItem = new JMenuItem("Save", saveIcon);
         exitMenuItem = new JMenuItem("Exit");
         fileMenu.add(newMenuItem);
         fileMenu.add(openMenuItem);
@@ -46,7 +50,7 @@ public class PlayerFormV11 extends PlayerFormV10 {
         colorMenu.add(blueMenuItem);
         colorMenu.add(customMenuItem);
 
-        JMenuItem sizeMenu = new JMenu("Size");
+        JMenu sizeMenu = new JMenu("Size"); // Corrected: Changed to JMenu
         size16MenuItem = new JMenuItem("16");
         size20MenuItem = new JMenuItem("20");
         size24MenuItem = new JMenuItem("24");
@@ -63,10 +67,33 @@ public class PlayerFormV11 extends PlayerFormV10 {
 
         setJMenuBar(menuBar);
     }
-    
+
     public void actionPerformed(ActionEvent e) {
         super.actionPerformed(e);
+    
+        if (e.getSource() == customMenuItem) {
+            Color newColor = JColorChooser.showDialog(this, "Choose Color", customMenuItem.getBackground());
+            if (newColor != null) {
+                nameTextField.setForeground(newColor);
+                nationalityTextField.setForeground(newColor);
+                dobTextField.setForeground(newColor);
+            }
+        }
     }
+
+    public void enableKeyboard() {
+        // Set mnemonic keys and accelerator keys
+        newMenuItem.setMnemonic(KeyEvent.VK_N);
+        openMenuItem.setMnemonic(KeyEvent.VK_O);
+        saveMenuItem.setMnemonic(KeyEvent.VK_S);
+        exitMenuItem.setMnemonic(KeyEvent.VK_X);
+
+        newMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        openMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        exitMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+    }
+    
 
     public void addListeners() {
         super.addListeners();
@@ -79,9 +106,9 @@ public class PlayerFormV11 extends PlayerFormV10 {
         playerForm.addMenus();
         playerForm.addListeners();
         playerForm.setFrameFeatures();
+        playerForm.enableKeyboard();
     }
 
-    // Main method to run the application for PlayerFormV8
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
