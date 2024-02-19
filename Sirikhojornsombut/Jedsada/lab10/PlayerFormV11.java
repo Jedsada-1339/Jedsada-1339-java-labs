b199f5553b9fd403afa5c3ed7a934b01cf9b9e84
Jedsada-1339 <jedsada.si@kkumail.com>
2024-02-19 14:16:29 +0700

    update lab10

--- Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
+++ Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
@@ -1,5 +1,8 @@
 package sirikhojornsombut.jedsada.lab10;
 
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
@@ -7,8 +10,56 @@ public class PlayerFormV11 extends PlayerFormV10 {
     protected JMenuItem customMenuItem;
 
     protected void addMenus(){
-        super.addMenus();
+        JMenuBar menuBar = new JMenuBar(); // Create a menu bar
+
+        // Menu "File" with four menu items
+        JMenu fileMenu = new JMenu("File");
+        ImageIcon newIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/New-icon.png");
+        newMenuItem = new JMenuItem("New",newIcon);
+        ImageIcon openIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/Open-icon.png");
+        openMenuItem = new JMenuItem("Open",openIcon);
+        ImageIcon saveIcon = new ImageIcon("sirikhojornsombut/jedsada/lab8/ICON/Save-icon.png");
+        saveMenuItem = new JMenuItem("Save",saveIcon);
+        exitMenuItem = new JMenuItem("Exit");
+        fileMenu.add(newMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(exitMenuItem);
+
+        // Menu "Config" with two menu items
+        JMenu configMenu = new JMenu("Config");
+
+        JMenu colorMenu = new JMenu("Color");
+        redMenuItem = new JMenuItem("Red");
+        greenMenuItem = new JMenuItem("Green");
+        blueMenuItem = new JMenuItem("Blue");
         customMenuItem = new JMenuItem("Custom");
+        colorMenu.add(redMenuItem);
+        colorMenu.add(greenMenuItem);
+        colorMenu.add(blueMenuItem);
+        colorMenu.add(customMenuItem);
+
+        JMenuItem sizeMenu = new JMenu("Size");
+        size16MenuItem = new JMenuItem("16");
+        size20MenuItem = new JMenuItem("20");
+        size24MenuItem = new JMenuItem("24");
+        sizeMenu.add(size16MenuItem);
+        sizeMenu.add(size20MenuItem);
+        sizeMenu.add(size24MenuItem);
+
+        configMenu.add(colorMenu);
+        configMenu.add(sizeMenu);
+
+        // Add menus to the menu bar
+        menuBar.add(fileMenu);
+        menuBar.add(configMenu);
+
+        setJMenuBar(menuBar);
+    }
+    
+    public void addListeners() {
+        super.addListeners();
+        customMenuItem.addActionListener(this);
     }
 
     public static void createAndShowGUI() {
