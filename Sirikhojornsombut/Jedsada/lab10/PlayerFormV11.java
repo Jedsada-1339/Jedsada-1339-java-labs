98c9f9bd12ce6cf7dd8093acc5e09abf78c444b6
Jedsada-1339 <jedsada.si@kkumail.com>
2024-02-20 18:05:37 +0700

    update lab10

--- Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
+++ Sirikhojornsombut/Jedsada/lab10/PlayerFormV11.java
@@ -78,7 +78,7 @@ public class PlayerFormV11 extends PlayerFormV10 {
 
 
         if (e.getSource() == customMenuItem) {
-            Color newColor = JColorChooser.showDialog(this, "Choose Text Color", customMenuItem.getBackground());
+            Color newColor = JColorChooser.showDialog(this, "Choose Text Color", nameTextField.getForeground());
             if (newColor != null) {
                 nameTextField.setForeground(newColor);
                 nationalityTextField.setForeground(newColor);
